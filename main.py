a6f1d444048bda1d43899b253719bd35f2353634
Abdellah lahnaoui <abdellah.lahnaoui@gmail.com>
2021-03-20 20:01:37 +0100

    Connected the json data to the app and implemented simple data retrieval

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 import json
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'b9m6z7Vv39WTc2'
 
 jsonFilePath = "data\Food_Table.json"
 
+def get_info(name):
+    with open(jsonFilePath) as jsonFile:
+        results = []
+        data = json.load(jsonFile)
+        for i in range(0,len(data)-1):
+            for item, value in data[str(i)].items():
+                if item == 'Display_Name':
+                    if name in value.lower():
+                        results.append(data[str(i)]['Display_Name']+' '+data[str(i)]['Portion_Display_Name'])
+                        break
+        return(results)
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
+    results = []
     if request.method == 'POST':
         name = request.form['name']
-    return render_template('index.html')
+        if not name:
+            flash('Name is required!')
+        else:
+            results = get_info(name)
+    return render_template('index.html', results = results)
 
 if __name__ == '__main__':
     app.run()
